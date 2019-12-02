eaeb8659b09efb5babee8618c0652fa3db112324
chen-96 <983100868@qq.com>
2019-12-02 22:08:08 +0800

    1

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            for (int a = 0; a <= 100; a++)
+            {
+                for (int b = 0; b <= 100; b++)
+                {
+                    for (int c = 0; c <= 100; c++)
+                    {
+                        int w = a * 5 + b * 3 + c / 3;
+                        int y = a + b + c;
+                        if (w == 100 && y == 100 && c % 3 == 0)
+                        {
+
+                            Console.WriteLine("{0}{1}{2}", a, b, c);
+                        }
+                    }
+                }
+            }
+            Console.Read();
         }
+        
     }
 }
